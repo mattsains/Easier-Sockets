20686e120ea7a31a8941835a159fb066bc21c361
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-11-04 11:54:01 +0200

    Seems pretty much done. Need to look at the code, and start a test harness

--- EasierSockets/Class1.cs
+++ EasierSockets/Class1.cs
@@ -61,14 +61,14 @@ namespace EasierSockets
         /// </summary>
         /// <param name="dispatch">A delegate accepting a string and sending a string</param>
         /// <param name="separator">what character/string signals the end of a message? eg newline, semicolon</param>
-        public void Listen(Dispatcher dispatch, char separator='\n')
+        public void Listen(Dispatcher dispatch, char separator = '\n')
         {
-            this.Listen(dispatch,separator.ToString());
+            this.Listen(dispatch, separator.ToString());
         }
-        public void Listen(Dispatcher dispatch, string separator="\n")
+        public void Listen(Dispatcher dispatch, string separator = "\n")
         {
             this.dispatcher = dispatch;
-            this.separator=separator;
+            this.separator = separator;
             Listener = new Thread(new ThreadStart(ListenAsync));
             Listener.Start();
         }
@@ -77,6 +77,9 @@ namespace EasierSockets
             //TODO: need to change this so sockets end cleanly.
             Listener.Abort();
         }
+        /// <summary>
+        /// Waits for clients to connect, then puts them on a new thread
+        /// </summary>
         private void ListenAsync()
         {
             lock (ServerSocket)
@@ -89,14 +92,63 @@ namespace EasierSockets
                 while (true)
                 {
                     Socket handle = ServerSocket.Accept();
-
+                    lock (clientThreads)
+                    {
+                        //send the client to a new WaitForClient
+                        clientThreads.Add(new Thread(new ParameterizedThreadStart(WaitForClient)));
+                        //I hate this. why can't List<T>.Add return the index of the added item? oh yeah, I remember, cascading. ugh.
+                        clientThreads[clientThreads.Count - 1].Start(handle);
+                    }
                 }
             }
         }
-
-        private void WaitForClient()
+        /// <summary>
+        /// talks to a single client. Waits for the separator, then calls the delegate
+        /// </summary>
+        /// <param name="o">The socket connected to the client, casted as object</param>
+        private void WaitForClient(object o)
         {
-
+            string s;
+            Dispatcher d;
+            lock (separator) lock (dispatcher)
+                {
+                    //cache some variables so we don't lock them for long
+                    d = dispatcher;
+                    s = separator;
+                }
+            Socket handle = (Socket)o;
+            //we're, like, fifty threads deep here and I'm scared :O
+            byte[] bytes = new byte[1024];
+            string data = "";
+            string response="";
+            int size;
+            // receive a stream, and let the dispatcher handle it if we get a separator
+            while (handle.Connected)
+            {
+                size = handle.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, size);
+                if (data.Contains(s))
+                {
+                    //we have a separator in the bufffer. Isolate it and send to the dispatcher
+                    string[] messages = data.Split(new string[1] { s }, StringSplitOptions.None);
+                    if (data.EndsWith(s))
+                    {
+                        foreach (string str in messages)
+                            if ((response=d(str))!="")
+                                handle.Send(Encoding.ASCII.GetBytes(response+s));
+                        data = "";
+                    }
+                    else
+                    {
+                        for (int i = 0; i < messages.Length - 1; i++)
+                            if ((response = d(messages[i])) != "")
+                                handle.Send(Encoding.ASCII.GetBytes(response + s));
+                        data = messages[messages.Length - 1];
+                    }
+                }
+            }
+            handle.Shutdown(SocketShutdown.Both);
+            handle.Close();
         }
     }
 }
