7b7b46cd5277506413d354eadce707d47e5c8efb
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-11-04 20:46:59 +0200

    client in a workable state

--- EasierSockets/Client.cs
+++ EasierSockets/Client.cs
@@ -10,6 +10,30 @@ namespace EasierSockets
 {
     public class ClientSock
     {
-        
+        Socket sock;
+        string separator;
+        public ClientSock(string host, int port, string separator = "\n")
+        {
+            IPHostEntry hostip = Dns.GetHostEntry(host);
+            IPAddress ip = hostip.AddressList[0];
+            IPEndPoint endpoint = new IPEndPoint(ip, port);
+            //TODO: Allow different modes like UDP
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(endpoint);
+            this.separator = separator;
+        }
+        public string send(string message)
+        {
+            byte[] tx = Encoding.ASCII.GetBytes(message + separator);
+            sock.Send(tx);
+            byte[] rx = { };
+            sock.Receive(rx);
+            return Encoding.ASCII.GetString(rx);
+        }
+        public ~ClientSock()
+        {
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
     }
 }
