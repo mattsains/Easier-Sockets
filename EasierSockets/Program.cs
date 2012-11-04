8ba162eb355986725da89f93336a9fcc123b4a75
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-11-04 12:15:13 +0200

    started test harness

---
+++ EasierSockets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasierSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sock sock = new Sock(1234);
+            Dispatcher disp = new Dispatcher(handler);
+            sock.Listen(disp);
+            Console.ReadLine();
+        }
+        static string handler(string message)
+        {
+            return string.Format("You sent: {0}", message);
+        }
+    }
+}
