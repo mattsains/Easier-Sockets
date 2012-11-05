4e63af171891ac4610f15114fb09cf5c7032aa70
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-11-05 14:34:23 +0200

    intial client tester

--- EasierSockets/Program.cs
+++ EasierSockets/Program.cs
@@ -9,7 +9,23 @@ namespace EasierSockets
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("host: ");
+            string server=Console.ReadLine();
+            Console.Write("port: ");
+            ClientSock cs = new ClientSock(server, int.Parse(Console.ReadLine()), "\n", new ServerDisconnect(disconn), new ServerMessage(msg));
+            while (true)
+            {
+                Console.Write(">> ");
+                cs.send(Console.ReadLine());
+            }
+        }
+        public static void disconn()
+        {
+            Environment.Exit(0);
+        }
+        public static void msg(string m)
+        {
+            Console.WriteLine("<< {0}", m);
         }
     }
 }
